efda67a8a5dc74a01a6a05b61db9371b4df6e296
noumanalisyed <nouman.ali.syed@gmail.com>
2023-11-14 13:41:31 +0500

    first commit

---
+++ prj-course-reg/src/main/java/org/example/entity/Course.java
@@ -0,0 +1,47 @@
+package org.example.entity;
+
+public class Course {
+    private Integer id;
+    private Strig name;
+    private Strig description;
+
+    public Course() {
+        id = 0;
+        name = "";
+        description = "";
+    }
+
+    public Course(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
+}
